840150539e1429a722044a33648c0d9a3a2ed0b2
CTW100 <sulsy887@naver.com>
2022-08-16 11:56:17 +0000

    auth middleware

--- budget_cal_api/models/User.js
+++ budget_cal_api/models/User.js
@@ -44,5 +44,19 @@ userSchema.methods.generateToken = function (cb) {
 	});
 };
 
+// auth 인증 -복호화 (토큰을 디코드)
+userSchema.methods.findByToken = function (token, cb) {
+	const user = this;
+
+	jwt.verify(token, 'secretToken', (err, decoded) => {
+		// 유저 아이디를 이용해서 유저를 찾은 다음에
+		// 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+		user.findOne({ _id: decoded, token: token }, (err, user) => {
+			if (err) return cb(err);
+			cb(null, user);
+		});
+	});
+};
+
 const User = mongoose.model('user', userSchema);
 module.exports = User;
